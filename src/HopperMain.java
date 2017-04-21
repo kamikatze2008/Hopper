b4256912af9e1e153246cd2e454045499fdb41c3
s.denysov <s.denysov@szg-tech.com>
2017-04-21 13:13:01 +0300

    counting optimisation

--- src/HopperMain.java
+++ src/HopperMain.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class HopperMain {
+    private static HashMap<String, Integer> combinationsHashMap = new HashMap<>();
+
     public static void main(String[] args) {
         int n, k;
         Scanner scanner = new Scanner(System.in);
@@ -22,14 +26,21 @@ public class HopperMain {
 
     private static int countCompositionRecursive(int n, int k) {
         int r = 0;
-        if (k == n - 1) {
-            r = (int) Math.pow(2, n - 1) - 1;
-        } else if (k >= n) {
-            r = (int) Math.pow(2, n - 1);
+        String keyString = "" + n + "," + k;
+        Integer value = combinationsHashMap.get(keyString);
+        if (value != null) {
+            r = value;
         } else {
-            for (int i = 1; i <= k; i++) {
-                r += countCompositionRecursive(n - i, k);
+            if (k == n - 1) {
+                r = (int) Math.pow(2, n - 1) - 1;
+            } else if (k >= n) {
+                r = (int) Math.pow(2, n - 1);
+            } else {
+                for (int i = 1; i <= k; i++) {
+                    r += countCompositionRecursive(n - i, k);
+                }
             }
+            combinationsHashMap.put(keyString, r);
         }
         return r;
     }
